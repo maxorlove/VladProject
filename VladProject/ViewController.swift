cdb2f78fba3ae3839ac61546029a3212d5cd5e38
Orlov Maxim <108331905+maxorlove@users.noreply.github.com>
2022-12-27 20:01:13 +0700

    Initial Commit

---
+++ VladProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VladProject
+//
+//  Created by Orlov Maxim on 27.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
