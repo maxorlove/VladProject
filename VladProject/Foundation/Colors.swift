bfb8e9944b903d46b9c655bf63c4e5accb1b404c
vlkitov <vlkitov@gmail.com>
2023-01-08 16:28:58 +0400

    Setup assets catalog

---
+++ VladProject/Foundation/Colors.swift
@@ -0,0 +1,20 @@
+//
+//  Colors.swift
+//  VladProject
+//
+//  Created by Vladislav Kitov on 08.01.2023.
+//
+
+import UIKit
+
+enum Colors {
+    
+//    static let accentColor = UIColor(named: "Accent")
+//    static let accentTextColor = UIColor(named: "AccentText")
+//    static let primaryBackgroundColor = UIColor(named: "PrimaryBackground")
+//    static let primaryTextColor = UIColor(named: "PrimaryText")
+//    static let secondaryBackgroundColor = UIColor(named: "SecondaryBackground")
+//    static let surfaceColor = UIColor(named: "Surface")
+//    static let tertiaryTextColor = UIColor(named: "TertiaryText")
+    
+}
---
+++ VladProject/Foundation/Typography.swift
@@ -0,0 +1,19 @@
+//
+//  Typography.swift
+//  VladProject
+//
+//  Created by Vladislav Kitov on 08.01.2023.
+//
+
+import UIKit
+
+enum FontSize {
+    
+    // Headers
+    
+    static let largeFont = UIFont.systemFont(ofSize: 171, weight: .thin)
+    static let mediumFont = UIFont.systemFont(ofSize: 30, weight: .regular)
+    static let regularFont = UIFont.systemFont(ofSize: 20, weight: .regular)
+    static let titleFont = UIFont.systemFont(ofSize: 17, weight: .semibold)
+
+}
